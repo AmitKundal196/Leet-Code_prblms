e4b92e9b46f56915d36114abae51c4f07bd369b9
AmitKundal196 <amitkundal196@gmail.com>
2026-03-28 23:31:04 +0530

    single number problem

---
+++ Single_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int val : nums) {
+            ans ^= val;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+
+   
+    vector<int> nums = {4, 1, 2, 1, 2};
+
+   
+    int result = sol.singleNumber(nums);
+
+   
+    cout << "Single number is: " << result << endl;
+
+    return 0;
+}
