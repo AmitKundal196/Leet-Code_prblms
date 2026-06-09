695b43bc7bdda04667868c4b578da33be4786aac
AmitKundal196 <amitkundal196@gmail.com>
2026-06-09 23:22:11 +0530

    sqauring of given array

---
+++ Arrays/Squareofarray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++) {
+            ans[i] = nums[i] * nums[i];
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums = {-4, -1, 0, 3, 10};
+
+    Solution obj;
+    vector<int> result = obj.sortedSquares(nums);
+
+    cout << "Output: ";
+    for(int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
