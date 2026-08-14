2fc99c019883137729a7e570e7b36fc880f6cd27
AmitKundal196 <amitkundal196@gmail.com>
2026-08-14 23:37:05 +0530

    checking is cycle exits or not

---
+++ LinkedList/hascycle.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+
+    // Creating: 1 -> 2 -> 3 -> 4
+    //                    ↑    |
+    //                    |____|
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    // Creating cycle: 4 -> 3
+    head->next->next->next->next = head->next->next;
+
+    Solution obj;
+
+    if (obj.hasCycle(head)) {
+        cout << "Cycle exists" << endl;
+    }
+    else {
+        cout << "No cycle" << endl;
+    }
+
+    return 0;
+}
